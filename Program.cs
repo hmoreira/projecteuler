5f3a4c123246dbb1b664c0084a4f0e598439f91d
Heber Moreira <heber@santoslab.com>
2024-07-22 21:05:43 -0300

    Commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace P3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 600851475143;
+            //var number = int.Parse(Console.ReadLine());
+            var divider = 2;
+
+            while (number > 1)
+            {
+                if (number % divider == 0)
+                {
+                    //Console.WriteLine(divider);
+                    number = number / divider;                    
+                }                
+                else
+                    divider++;
+            }
+            Console.WriteLine(divider);
+        }
+    }
+}
